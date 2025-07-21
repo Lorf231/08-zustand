350967d5206ec38258af67474828e67a6ef3ec94
Roman Fliunt <romanflyn@gmail.com>
2025-07-21 15:14:37 +0300

    fixed error

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,9 +1,4 @@
-"use client";
-
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
-import css from "./not-found.module.css";
-import Link from "next/link";
+import css from "./Home.module.css";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -25,21 +20,12 @@ export const metadata: Metadata = {
 };
 
 const NotFound = () => {
-  const router = useRouter();
-
-  useEffect(() => {
-    const timer = setTimeout(() => router.push("/"), 3000);
-    return () => clearTimeout(timer);
-  }, [router]);
-
   return (
     <div>
       <h1 className={css.title}>404 - Page not found</h1>
       <p className={css.description}>
-        Sorry, the page you are looking for does not exist. You`ll go to the
-        home page in few seconds or you can push the button!
+        Sorry, the page you are looking for does not exist.
       </p>
-      <Link href="/">Go back home</Link>
     </div>
   );
 };
